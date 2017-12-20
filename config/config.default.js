6f3330a40337b1333d0f079907e83699ae29fcc4
Guo Jun <guo_j@worksap.co.jp>
2017-12-20 21:24:32 +0800

    init Diting

---
+++ config/config.default.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1513774152400_3296';
+
+  // add your config here
+  config.middleware = [];
+
+  // mongo config
+  config.mongo = {
+    host: '',
+    port: ''
+  };
+
+  // redis config
+  config.redis = {
+    host: '',
+    port: ''
+  };
+
+  return config;
+};
