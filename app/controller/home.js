b44c9dcaf3754b62c2719b7ec6af27f31aaf2382
Unknown <nemomojie@gmail.com>
2017-12-21 01:27:13 +0800

    hello world

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,10 +1,26 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+const Vue = require('vue');
+const renderer = require('vue-server-renderer').createRenderer();
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'hi, egg';
+      const app = new Vue({
+          data: {
+              name: 'Momojie'
+          },
+          template: `<div>Hello: {{ name }}</div>`
+      });
+      renderer.renderToString(app, (err, html) => {
+          this.ctx.res.end(`
+      <!DOCTYPE html>
+      <html lang="en">
+        <head><title>Hello</title></head>
+        <body>${html}</body>
+      </html>
+    `);
+      })
   }
 }
 
